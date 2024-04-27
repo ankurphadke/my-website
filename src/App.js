fb8ba5e5388d18489d19ad7ff5cc7de9bea45a1d
Ankur Phadke <ankurphadke@gmail.com>
2024-04-27 00:06:35 -0700

    Unscrapped version

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { BrowserView } from "react-device-detect";
 
 import CompNavbar from './components/Navbar';
+import CompWelcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <h1 className="Name">Ankur Phadke</h1>
-      <BrowserView>
-        <CompNavbar />
-      </BrowserView>
+      <CompNavbar />
+      <CompWelcome />
 
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import './Navbar.css';
-
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap';
 
 function CompNavbar() {
   return (
-    <Navbar>
-        <Nav className="Navbar">
-            <Nav.Link href="">About Me</Nav.Link>
-            <Nav.Link href="">Music</Nav.Link>
-        </Nav>
+    <Navbar className="Navbar">
+      <h1 className="Name">Ankur Phadke</h1>
+      <Nav id="basic-navbar-nav">
+        <Nav.Link href="/about">About Me</Nav.Link>
+        <Nav.Link href="/music">Music</Nav.Link>
+        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav>
     </Navbar>
-  )
+  );
 }
 
 export default CompNavbar;
