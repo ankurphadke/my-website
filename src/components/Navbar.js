5fec4b1bb28ebce682eebe6c20f5ee581c581ada
Ankur Phadke <ankurphadke@gmail.com>
2024-05-06 23:58:45 -0700

    Change Navbar text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import { Nav, Navbar } from 'react-bootstrap';
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
 function CompNavbar() {
   return (
     <Navbar className="Navbar">
       <Nav>
-        <Nav.Link className="NavLink" href="/about">Ankur Phadke</Nav.Link>
+        <Nav.Link className="NavLinkName" href="/home">Ankur Phadke</Nav.Link>
+        <Nav.Link className="NavLink" href="/work">Work</Nav.Link>
+        <Nav.Link className="NavLink" href="/school">School</Nav.Link>
         <Nav.Link className="NavLink" href="/music">Music</Nav.Link>
-        <Nav.Link className="NavLink" href="/portfolio">Portfolio</Nav.Link>
-        <Nav.Link className="NavLink" href="/contact">Contact</Nav.Link>
+        <Nav.Link className="NavLink" href="/interests">Interests</Nav.Link>
+        <Nav.Link className="NavLink" href="/resume">Resume</Nav.Link>
       </Nav>
     </Navbar>
   );
