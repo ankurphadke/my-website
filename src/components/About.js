de9fc1bf85810f08517379b8a5f25e6cc2e87fda
Ankur Phadke <ankurphadke@gmail.com>
2024-05-07 23:16:03 -0700

    Add contact links

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,11 @@ function CompAbout() {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut quam vel felis sodales rhoncus. In hac habitasse platea dictumst. Nulla facilisi. Donec sit amet sapien sed purus lacinia gravida. Sed eu enim turpis. Duis vehicula felis vel risus fermentum, eu tincidunt urna fermentum. In a velit sit amet odio dignissim scelerisque. Vivamus vestibulum risus non suscipit vulputate.
       </p>
+      <div className="Contacts">
+        <a className="ContactLink" href="mailto:aphadke@uwaterloo.ca" target="_blank">E-mail</a>
+        <a className="ContactLink" href="https://www.linkedin.com/in/ankur-phadke/" target="_blank">LinkedIn</a>
+        <a className="ContactLink" href="https://github.com/ankurphadke" target="_blank">GitHub</a>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Nav, Navbar } from 'react-bootstrap';
-import Resume from 'public/resume.pdf'
+import Resume from 'public/resume.pdf';
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
