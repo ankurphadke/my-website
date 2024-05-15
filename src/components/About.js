d943837606d0b0e84212fe1ed862143611b91ca7
Ankur Phadke <ankurphadke@gmail.com>
2024-05-15 01:23:38 -0700

    Add contacts

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,12 @@ function CompAbout() {
     <div className="About">
       <h1 className="Name">Hi, I'm Ankur!</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut quam vel felis sodales rhoncus. In hac habitasse platea dictumst. Nulla facilisi. Donec sit amet sapien sed purus lacinia gravida. Sed eu enim turpis. Duis vehicula felis vel risus fermentum, eu tincidunt urna fermentum. In a velit sit amet odio dignissim scelerisque. Vivamus vestibulum risus non suscipit vulputate.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
-      <div className="Contacts">
-        <a className="ContactLink" href="mailto:aphadke@uwaterloo.ca" target="_blank">E-mail</a>
-        <a className="ContactLink" href="https://www.linkedin.com/in/ankur-phadke/" target="_blank">LinkedIn</a>
-        <a className="ContactLink" href="https://github.com/ankurphadke" target="_blank">GitHub</a>
-      </div>
+      <p>
+        You can get in touch at <a href="mailto:aphadke@uwaterloo.ca"target="_blank">aphadke@uwaterloo.ca</a>
+        , or via <a href="https://www.linkedin.com/in/ankur-phadke/" target="_blank">LinkedIn</a>.
+      </p>
     </div>
   )
 }
