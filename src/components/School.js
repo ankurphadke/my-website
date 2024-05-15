a99053f9c3daf86ec06deef84e501186006c86bb
Ankur Phadke <ankurphadke@gmail.com>
2024-05-15 01:23:20 -0700

    Add section descriptions

--- src/components/School.js
+++ src/components/School.js
@@ -1,9 +1,36 @@
 function CompSchool() {
   return (
     <div className="School">
+      <h2>
+        School
+      </h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut quam vel felis sodales rhoncus. In hac habitasse platea dictumst. Nulla facilisi. Donec sit amet sapien sed purus lacinia gravida. Sed eu enim turpis. Duis vehicula felis vel risus fermentum, eu tincidunt urna fermentum. In a velit sit amet odio dignissim scelerisque. Vivamus vestibulum risus non suscipit vulputate.
+        I recently graduated with a <b>Bachelor of Computer Science, Data
+        Science </b> from the <b>University of Waterloo</b> (ON, Canada) in
+        December 2023. My cummulative average was <b>86.5%</b>. Below are a few
+        extra-curricular highlights from my time at Waterloo.
       </p>
+      <h3>
+        Waterloo Data Science Club
+      </h3>
+      <h3>
+        UW Poker Studies Club
+      </h3>
+      <h3>
+        Intramural Sports
+      </h3>
+      <h3>
+        Hackathons
+      </h3>
+      <p>
+        Melting Pot Contest, ASA DataFest, Waterloo.AI Data Challenge, TOHacks
+      </p>
+      <h3>
+        WATonomous
+      </h3>
+      <h3>
+        Research
+      </h3>
     </div>
   )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,37 @@
 function CompWork() {
   return (
     <div className="Work">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut quam vel felis sodales rhoncus. In hac habitasse platea dictumst. Nulla facilisi. Donec sit amet sapien sed purus lacinia gravida. Sed eu enim turpis. Duis vehicula felis vel risus fermentum, eu tincidunt urna fermentum. In a velit sit amet odio dignissim scelerisque. Vivamus vestibulum risus non suscipit vulputate.
-      </p>
+    <h2>
+      Work
+    </h2>
+    <p>
+      I am currently working at Arista Networks as a Software Engineer.
+    </p>
+    <p>
+      I was part of the AI/ML team at Versa Networks, developing the VersaAI
+      suit of products. I worked on the development of the data platform,
+      devops, as well as anomaly detection.
+    </p>
+    <p>
+      Previously, I was in the finance industry working at two of the world's
+      biggest hedge funds in Point72 Asset Management and Balyasny Asset
+      Management. I primarily worked on time series problems, platform
+      engineering, and research in space of macro, commodities, and alternative
+      data.
+    </p>
+    <p>
+      I interned as a Product Data Analyst at Square Enix, working on AR mobile
+      games. I learnt a lot about A/B testing, user journeys, user segmentation,
+      and product analytics during this experience. I also interned at Intact
+      Financial, where I was on the Claims AI team developing machine learning
+      models to streamline the accident claims process.
+    </p>
+    <p>
+      I had the opportunity to work in a research role at the University of
+      Waterloo, under the supervision of Professor Grant Weddell. I worked on
+      a compiler for SQLP, an extension of SQL utilizing an abstract relational
+      model and attribute paths.
+    </p>
     </div>
   )
 }
